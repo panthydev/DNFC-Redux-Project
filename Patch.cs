322993f5b20eee8d5ee21e4ec7a43dc07fb3a4b9
Elyriand21 <felix.banks02@gmail.com>
2026-02-14 17:55:00 -0500

    Initial commit

---
+++ Patch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using MelonLoader;
+
+namespace UDNFC_Patch
+{
+    public class Patch : MelonMod
+    {
+        public override void OnEarlyInitializeMelon()
+        {
+            MelonLogger.Msg("From everyone at the DNFC Redux Project, we thank you for choosing our mod!");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            MelonLogger.Msg($"Scene {sceneName} was loaded with build index {buildIndex}.");
+        }
+    }
+}
