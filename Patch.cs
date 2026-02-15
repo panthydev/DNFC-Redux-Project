482eeb90d39c17440e74876469b279ca4b485bfa
Elyriand21 <felix.banks02@gmail.com>
2026-02-15 16:40:27 -0500

    Changed when components are loaded to ensure they're fully loaded

--- Patch.cs
+++ Patch.cs
@@ -2,12 +2,17 @@
 using MelonLoader;
 using System.Collections;
 using DNFC_Redux_Library;
+using UnityEngine.InputSystem;
+
+// 10ms is wait time
 
 namespace Patch
 {
     public class Patch : MelonMod
     {
+        int ms = 0;
         public readonly Library DNFC_Lib = new Library();
+        public bool componentsSearched = false;
 
         public override void OnEarlyInitializeMelon()
         {
@@ -22,15 +27,17 @@ namespace Patch
     suggestions, or want to contribute, feel free to join our Discord server!");
         }
 
-        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
         {
             if (sceneName == "MainMenu")
             {
                 DNFC_Lib.SetIsInMainMenu(true);
+                MelonLogger.Msg($"IsInMainMenu: {DNFC_Lib.IsInMainMenu()}");
             }
             else if (sceneName == "Loading")
             {
                 DNFC_Lib.SetIsInMainMenu(false);
+                MelonLogger.Msg($"IsInLoading: {DNFC_Lib.IsInLoading()}");
                 DNFC_Lib.SetIsInLoading(true);
             }
             else if (sceneName == "CityGameplay")
@@ -39,16 +46,41 @@ namespace Patch
                 {
                     DNFC_Lib.SetInitialized(true);
                     MelonLogger.Msg("Mod has been initialized");
-                }
-                DNFC_Lib.FindSettingsManagerComponent();
-
-                // Check if the SettingsManager GameObject was found
-                if (DNFC_Lib.GetSettingsManagerComponent() == null)
-                {
-                    MelonLogger.Error("SettingsManager GameObject not found in the scene!");
-                    return;
+                    
+                    MelonLogger.Msg("Sleeping finished, starting to find components");
                 }
             }
+        }
+
+        public override void OnUpdate()
+        {
+            // If the player is in the game and the mod is initialized
+            if (DNFC_Lib.IsInitialized() == true)
+            {
+                // This section is critical to ensure all components are loaded before reading them in
+                {
+                    if (ms != 11)    // Wait 11ms before finding components to ensure game has loaded them in.
+                    {
+                        ms++;
+                    }
+
+                    if (ms == 11 && componentsSearched == false)
+                    {
+                        DNFC_Lib.FindCharactersInUse();
+                        DNFC_Lib.FindSettingsManagerComponent();
+                        componentsSearched = true;
+                    }
+                }
+                if (Keyboard.current.zKey.wasPressedThisFrame)
+                {
+                    // Debug for checking if values are as expected
+                }
+            }
+        }
+
+        public override void OnSceneWasUnloaded(int buildIndex, string sceneName)
+        {
+            MelonLogger.Msg($"Scene Unloaded: {sceneName}");
         }
     }
 }
